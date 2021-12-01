b87223691b5ccfc711e26397368fefc23bc5ab08
JOANITA-51 <nakityojoanita51@gmail.com>
2021-12-01 06:23:08 +0300

    questions model

--- backend/models/questions.js
+++ backend/models/questions.js
@@ -1,2 +1,42 @@
 const mongoose = require('mongoose');
-const 
+const user = require('./user');
+const questionsSchema = new mongoose.Schema ({
+    createdAt: {
+        type: Date,
+        default:Date.now()
+    },
+    question: String,
+    author: String,
+    timelimit:Time,
+    category:String,
+    platform:{
+        name:String
+    },
+    score:Number,
+    expiresAt:{
+        type:Date
+    },
+    allowedStudents:[ ],
+    testNo:Number,
+    testTitle:String,
+    responses:{
+        createdAt: {
+            type: Date,
+            default:Date.now()
+        },
+        question_id:questions._id,
+        student_id:user._id,
+        response:String,
+        status:String,
+        duration:Time,
+        score:Number,
+        Reviewer:String,
+        Remarks:String,
+        Rating: String
+    },
+    Answers:{
+        question_id:questions._id,
+        pontentialAnswers:[ ]
+    }
+
+})
